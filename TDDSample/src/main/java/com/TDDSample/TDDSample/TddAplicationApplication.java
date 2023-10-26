c525b999ced01c525477d9061c4374fdcd466e1a
Renato Hioji <okamoto.hioji@gmal.com>
2023-10-26 16:01:55 -0300

    Main: First Commit

---
+++ TDDSample/src/main/java/com/TDDSample/TDDSample/TddAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.TDDSample.TDDSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAplicationApplication.class, args);
+	}
+
+}
---
+++ TDDSample/src/test/java/com/TDDSample/TDDSample/TddAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TDDSample.TDDSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
