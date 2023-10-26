6905c63f54f6d26e6d142b6028a5cf96f39cd1aa
Renato Hioji <okamoto.hioji@gmal.com>
2023-10-26 16:12:29 -0300

    Developmet: Initial Configuration

---
+++ TDDSample/src/test/java/com/TDDSample/TDDSample/WalletAppTests.java
@@ -0,0 +1,15 @@
+package com.TDDSample.TDDSample;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class WalletAppTests {
+        @Test
+        public void helloWorld(){
+            System.out.println("Hello World!");
+        }
+
+}
